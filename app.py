9fe60aa7393b5b2895708af66558e8f303307b44
Bernardo Martorano <bernardormartorano@gmail.com>
2025-05-08 15:30:04 -0300

    adding login route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
@@ -13,6 +13,13 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -23,6 +30,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+            login_user(user)
             return jsonify({'message': 'Login successful!'}), 200
     return jsonify({'message': 'Invalid Credencials!'}), 400
 
